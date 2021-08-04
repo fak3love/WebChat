48a790698deec30049d43780ee37be04dee908ca
neteromode <faust8king@gmail.com>
2021-08-04 10:32:02 +0300

    feature(api) added extensions and middlewares

---
+++ WebChat.Api/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using WebChat.Api.Middlewares;
+
+namespace WebChat.Api.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHadnler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
---
+++ WebChat.Api/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using WebChat.Application.Common.Exceptions;
+
+namespace WebChat.Api.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                await HandleExceptionAsync(context, exception);
+
+                _logger.LogError(exception.Message);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            string result = string.Empty;
+            var response = context.Response;
+            response.ContentType = "application/text";
+
+            switch (exception)
+            {
+                case ValidationException:
+                    response.ContentType = "application/json";
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    result = JsonConvert.SerializeObject((exception as ValidationException).Failures);
+
+                    break;
+                case NotFoundException:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    result = exception.Message;
+
+                    break;
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            if (result == string.Empty)
+                result = JsonConvert.SerializeObject(new { Error = exception.Message });
+
+            await response.WriteAsync(result);
+        }
+    }
+}
