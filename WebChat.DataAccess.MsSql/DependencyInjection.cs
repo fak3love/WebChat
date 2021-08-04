a9760f671cea5fe384b703ccb9a67615f815cf79
neteromode <faust8king@gmail.com>
2021-08-04 10:25:21 +0300

    feature(data-access.mssql) added dependency-injenction

---
+++ WebChat.DataAccess.MsSql/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebChat.DataAccess.MsSql
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessMsSql(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<WebChatContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("WebChatDatabase")));
+
+            return services;
+        }
+    }
+}
