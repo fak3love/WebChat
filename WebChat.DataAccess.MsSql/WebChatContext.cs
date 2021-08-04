1d0c537499c0723a96df6a7ddc77f93c66f67524
neteromode <faust8king@gmail.com>
2021-08-04 10:23:24 +0300

    feature(data-access.mssql) added dbcontext

---
+++ WebChat.DataAccess.MsSql/WebChatContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WebChat.Domain.Entities;
+using WebChat.Domain.Interfaces;
+
+namespace WebChat.DataAccess.MsSql
+{
+    public class WebChatContext : IdentityDbContext<User>
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<UserFriendStatus> FriendStatuses { get; set; }
+        public DbSet<UserLanguage> UserLanguages { get; set; }
+        public DbSet<UserFriend> UserFriends { get; set; }
+        public DbSet<UserMessage> UserMessages { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<UserPhoto> UserPhotos { get; set; }
+        public DbSet<UserPhotoLike> UserPhotoLikes { get; set; }
+        public DbSet<UserPhotoComment> UserPhotoComments { get; set; }
+        public DbSet<UserPhotoCommentLike> UserPhotoCommentLikes { get; set; }
+
+        public WebChatContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        public WebChatContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(WebChatContext).Assembly);
+            modelBuilder.Seed();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            ApplyAuditable();
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void ApplyAuditable()
+        {
+            foreach (var entry in ChangeTracker.Entries<IAuditable>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedAt = DateTime.UtcNow;
+                        break;
+                }
+            }
+        }
+    }
+}
