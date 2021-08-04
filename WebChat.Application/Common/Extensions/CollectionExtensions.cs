0717af869c27fb09a6a0af0e11bb342c15ff6f78
neteromode <faust8king@gmail.com>
2021-08-04 10:30:46 +0300

    feature(application) added behaviours, exceptions, extensions

---
+++ WebChat.Application/Common/Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WebChat.Application.Common.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static ICollection<T> Join<T>(this ICollection<T> collection, ICollection<T> joinCollection)
+        {
+            var newList = new List<T>(collection);
+            newList.AddRange(joinCollection);
+
+            return newList;
+        }
+    }
+}
