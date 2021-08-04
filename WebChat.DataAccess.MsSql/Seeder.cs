a3cbe1a15f9ee5583b06c26309ba1a0276961c11
neteromode <faust8king@gmail.com>
2021-08-04 10:24:04 +0300

    feature(data-access.mssql) added seeder

---
+++ WebChat.DataAccess.MsSql/Seeder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebChat.Domain.Collections;
+using WebChat.Domain.Entities;
+
+namespace WebChat.DataAccess.MsSql
+{
+    public static class Seeder
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(Countries.Value);
+            builder.Entity<City>().HasData(Cities.Value);
+            builder.Entity<Language>().HasData(Languages.Value);
+            builder.Entity<UserFriendStatus>().HasData(UserFriendStatuses.Value);
+        }
+    }
+}
