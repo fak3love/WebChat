6848efd6471f8f1bf6bda9232b8cc1fee6153741
neteromode <faust8king@gmail.com>
2021-08-20 19:31:21 +0300

    feature(data-access) added project, dependency-injection, file-manager

---
+++ WebChat.DataAccess/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebChat.Domain.Interfaces.Services;
+
+namespace WebChat.DataAccess
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, string localStoragePath)
+        {
+            services.AddScoped<IFileManager, LocalFileManager>(conf => new LocalFileManager(localStoragePath));
+
+            return services;
+        }
+    }
+}
---
+++ WebChat.DataAccess/LocalFileManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using WebChat.Domain.Interfaces.Services;
+
+namespace WebChat.DataAccess
+{
+    public class LocalFileManager : IFileManager
+    {
+        public string RootPath { get; }
+
+        public LocalFileManager(string rootPath)
+        {
+            RootPath = rootPath;
+        }
+
+        public async Task<byte[]> ReadAllBytes(string fileName)
+        {
+            try
+            {
+                return await File.ReadAllBytesAsync(Combine(fileName));
+            }
+            catch (Exception) { throw new Exception("Bad request"); }
+        }
+        public async Task<bool> WriteAllBytes(string fileName, byte[] bytes)
+        {
+            try
+            {
+                await File.WriteAllBytesAsync(Combine(fileName), bytes);
+                return true;
+            }
+            catch (Exception) { throw new Exception("Bad request"); }
+        }
+
+        public async Task<bool> Delete(string fileName)
+        {
+            try
+            {
+                return await Task.Run(() =>
+                {
+                    File.Delete(Combine(fileName));
+                    return true;
+                });
+            }
+            catch (Exception) { throw new Exception("Bad request"); }
+        }
+
+        private string Combine(string path) => Path.Combine(RootPath, path);
+    }
+}
