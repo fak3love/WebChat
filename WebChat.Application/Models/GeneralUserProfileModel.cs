b1da5a61173264cccbbe0b64591546de1e6ce830
neteromode <faust8king@gmail.com>
2021-08-20 19:35:28 +0300

    feature(application) added models and services

---
+++ WebChat.Application/Models/GeneralUserProfileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebChat.Application.Models
+{
+    public class GeneralUserProfileModel
+    {
+        public int Id { get; set; }
+        public int? CityId { get; set; }
+        public int? AvatarId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StatusMessage { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ WebChat.Application/Services/JwtGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebChat.Domain.Entities;
+using WebChat.Domain.Interfaces.Services;
+
+namespace WebChat.Application.Services
+{
+    public class JwtGenerator : IJwtGenerator
+	{
+		private readonly SymmetricSecurityKey _key;
+
+		public JwtGenerator(IConfiguration config)
+		{
+			_key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+		}
+
+		public string CreateToken(User user)
+		{
+			var claims = new List<Claim>()
+			{
+				new Claim(ClaimTypes.NameIdentifier, user.Id),
+				new Claim(ClaimTypes.Name, user.UserName)
+			};
+
+			var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var tokenDescriptor = new SecurityTokenDescriptor()
+			{
+				Subject = new ClaimsIdentity(claims),
+				Expires = DateTime.Now.AddDays(7),
+				SigningCredentials = credentials,
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+
+			return tokenHandler.WriteToken(token);
+		}
+	}
+}
