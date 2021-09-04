0a23c2d7485f081b1801e95b3c0870d7288ffa34
netero <faust8king@gmail.com>
2021-09-04 21:36:18 +0300

    refactor(react) project

--- webchat-react/src/App.tsx
+++ webchat-react/src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+import {Layout} from "./components/Layout";
+import {Profile} from "./pages/Profile";
+import {Authorization} from "./pages/Authorization";
+import {Registration} from "./pages/Registration";
+import {Settings} from "./pages/Settings";
+import {Friends} from "./pages/Friends";
+import {Messages} from "./pages/Messages";
+import {Photos} from "./pages/Photos";
+import {Notifications} from "./pages/Notifications";
 import './App.css';
+import './assets/fonts.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Layout>
+                    <Route exact path="/Profile/:id" component={Profile} />
+                    <Route exact path='/Authorization' component={Authorization} />
+                    <Route exact path='/Registration' component={Registration} />
+                    <Route exact path='/Settings' component={Settings} />
+                    <Route exact path='/Friends' component={Friends} />
+                    <Route exact path='/Messages' component={Messages} />
+                    <Route exact path='/Photos' component={Photos} />
+                    <Route exact path='/Notifications' component={Notifications} />
+                </Layout>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
