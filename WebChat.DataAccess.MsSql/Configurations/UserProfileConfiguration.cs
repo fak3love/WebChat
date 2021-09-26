19a18577ae057478b08fbd94c928afc836793bfe
netero <faust8king@gmail.com>
2021-09-26 22:32:13 +0300

    refactor(data-access.mssql) changed has default value

--- WebChat.DataAccess.MsSql/Configurations/UserProfileConfiguration.cs
+++ WebChat.DataAccess.MsSql/Configurations/UserProfileConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
 using WebChat.Domain.Entities;
 
 namespace WebChat.DataAccess.MsSql.Configurations
@@ -15,7 +14,7 @@ namespace WebChat.DataAccess.MsSql.Configurations
             builder.Property(prop => prop.LastName).HasMaxLength(30).IsRequired();
             builder.Property(prop => prop.StatusMessage).HasMaxLength(200);
             builder.Property(prop => prop.Gender).HasMaxLength(6);
-            builder.Property(prop => prop.LastActionDate).HasDefaultValue(DateTime.Now);
+            builder.Property(prop => prop.LastActionDate).HasDefaultValueSql("GETDATE()");
         }
     }
 }
